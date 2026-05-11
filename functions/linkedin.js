73f119cfeacb6f2f99da7430de18fa84f5ac1d0c
fortitudefx <salmankhanfx@gmail.com>
2026-05-11 21:18:44 +0400

    Remove 'LinkedIn-Version' header from request
    Removed the 'LinkedIn-Version' header from the request.

--- functions/linkedin.js
+++ functions/linkedin.js
@@ -73,7 +73,6 @@ export async function onRequestPost(context) {
         'Authorization': `Bearer ${ACCESS_TOKEN}`,
         'Content-Type': 'application/json',
         'X-Restli-Protocol-Version': '2.0.0',
-        'LinkedIn-Version': '202401'
       },
       body: JSON.stringify(payload)
     });
